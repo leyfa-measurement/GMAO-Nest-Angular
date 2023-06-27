bfb8078d27e51a9aa2bf50a9d9ba00c2c01fb284
Sebastien-Brmt <68854338+Sebastien-Brmt@users.noreply.github.com>
2023-06-27 16:10:53 +0200

    init config file

--- gmao-backend/src/app.controller.ts
+++ gmao-backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService  } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly configService: ConfigService) {}
 
   @Get()
   getHello(): string {
+    this.configService.get('APP_PORT');
     return this.appService.getHello();
   }
 }
--- gmao-backend/src/app.module.ts
+++ gmao-backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
